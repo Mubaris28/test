4d49b6e53e532d00e4bc760715e98fd573e2bea6
muhammed mubaris <mubarismuhammed33@gmail.com>
2023-10-24 11:12:23 +0530

    test

---
+++ assets/js/custom.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var myCarousel = document.getElementById('carouselExampleIndicators');
+    var carousel = new bootstrap.Carousel(myCarousel, {
+        interval: false, // Set the interval to false to stop the automatic sliding
+    });
+});
+
+
+
+
+
+
+
